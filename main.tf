e746f7f896a5624ab58dba449e53d505045945c4
beastsAI <reddy.nikhit@gmail.com>
2023-09-26 17:50:32 -0500

    terraform provider info updated

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 # Define the AWS provider configuration
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1" # Specify your desired AWS region
+
 }
 
 # Create a VPC
